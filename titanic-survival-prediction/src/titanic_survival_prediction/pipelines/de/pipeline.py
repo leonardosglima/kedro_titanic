506ea8dc22dada479d2dfff7702a6a5ca51361af
Leonardo Lima <leonardo.lima@bitka.com.br>
2025-08-29 18:57:02 -0300

    Criação do pipeline de Engenharia de Dados

--- titanic-survival-prediction/src/titanic_survival_prediction/pipelines/de/pipeline.py
+++ titanic-survival-prediction/src/titanic_survival_prediction/pipelines/de/pipeline.py
@@ -3,8 +3,25 @@ This is a boilerplate pipeline 'de'
 generated using Kedro 1.0.0
 """
 
-from kedro.pipeline import Node, Pipeline  # noqa
+# src/titanic_survival_prediction/pipelines/de/pipeline.py
 
+from kedro.pipeline import Pipeline, node
+from kedro.pipeline.modular_pipeline import pipeline
+from .nodes import preprocess_titanic_data, split_data
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([])
+    return pipeline(
+        [
+            node(
+                func=preprocess_titanic_data,
+                inputs="titanic_raw_data",
+                outputs="preprocess_titanic_node",
+            ),
+            node(
+                func=split_data,
+                inputs=["preprocessed_titanic", "params:data_science"],
+                outputs=["X_train", "X_test", "y_train", "y_test"],
+                name="split_data_node",
+            )
+        ]
+    )
